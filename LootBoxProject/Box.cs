d055facdb4e91e4e47b5b6a47b81f67be0809b5a
Dennis Kafton <boom1axe@att.net>
2019-08-07 15:30:43 -0700

    Added docoumentaton

--- LootBoxProject/Box.cs
+++ LootBoxProject/Box.cs
@@ -8,10 +8,20 @@ namespace LootBoxProject
 {
     class Box
     {
+        // theme/tag of the lootbox
         public string LootboxTheme { get; set; }
+
+        // Title/Name of the box
         public string LootboxName { get; set; }
+
+        // ???
         public string LootboxContents { get; set; }
+
+        // A description explaining the consept an contents of the lootbox
         public string LootboxDescription { get; set; }
+
+        // The stock price of the lootbox
+        // DO NOT CHANGE FOR sales, discounts, or limited time offers
         public double LootboxPrice { get; set; }
     }
 }
